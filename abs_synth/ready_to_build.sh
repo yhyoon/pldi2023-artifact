d6337d69606ec1eef89bf8fd419c00cdfe608040
yoony <yoony@sparrowfasoo.com>
2023-03-08 13:46:33 +0900

    better build

--- abs_synth/ready_to_build.sh
+++ abs_synth/ready_to_build.sh
@@ -1,19 +1,17 @@
+#!/bin/bash
+
 if [ ! -d "$HOME/.opam" ]
 then
-    opam init
-else
-    echo "opam already initialized"
+    opam init --auto-setup --disable-sandboxing --yes --compiler=$OCAML_VERSION && opam clean
 fi
 
-if [[ ! -d "$HOME/.opam/abs_synth" ]]
+if [ ! -d "$HOME/.opam/abs_synth" ]
 then
     opam switch create abs_synth 4.12.0
-else
-    echo "switch 'abs_synth' already exists"
 fi
 
 opam switch abs_synth
 opam install --yes dune merlin ocaml-lsp-server dune-build-info batteries ocamlgraph core_kernel yojson containers-data containers z3
 
-# if z3 install failed, install z3.4.8.5 instead of latest
+# NOTE: if z3 install failed, install z3.4.8.5 instead of latest
 # opam install --yes z3.4.8.5
