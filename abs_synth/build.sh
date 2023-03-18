d61dc0a340f55c019af67b631687dd49143d0ac5
yoony <yoony@sparrowfasoo.com>
2023-03-18 16:48:22 +0900

    get tools from open repository

--- abs_synth/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dune build
-dune install --prefix=_install
-cp _install/bin/abs_synth abs_synth.exe
--- probe/src/main/scala/ast/Types.scala
+++
@@ -1,6 +0,0 @@
-package ast
-
-object Types extends Enumeration {
-  type Types = Value
-  val String, Int, Real, Bool, BitVec64 = Value
-}
